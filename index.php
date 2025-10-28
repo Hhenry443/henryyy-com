9233f742ecc9c034fa969d1329a63b9defdaf209
Henry Barnes <henrytbarnes07@gmail.com>
2025-10-28 00:35:10 +0000

    Added landing page and temp page

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
 <html>
 
 <head>
-    <title>Page Title</title>
+    <title>This is a landing page</title>
+    <link href="./public/css/output.css" rel="stylesheet">
+    <meta http-equiv="refresh" content="0;url=./public/home.php" />
 </head>
 
-<body>
+<body class="bg-[#F2F0EF]">
 
-    <h1>This is a Heading</h1>
-    <p>This is a paragraph.</p>
-    <div>
-        <p>Hello World!</p>
+    <div class='w-full h-screen flex flex-col items-center justify-center text-xl space-y-2 text-gray-600'>
+        <p>Redirecting...</p>
     </div>
 
 </body>
---
+++ public/home.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <link href="./css/output.css" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Serif:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+  <title>Henry's Webhome</title>
+</head>
+
+<body class="bg-[#F2F0EF]">
+
+  <div class='w-full h-screen flex flex-col items-center justify-center text-xl space-y-2 text-gray-600'>
+    <p class="text-6xl">🚧</p>
+    <h1 class="font-semibold">Hello!</h1>
+    <p>This is <span class="font-noto-serif">going</span> to be the <span class='text-blue-500 font-bold font-noto-serif'>BEST</span> website!</p>
+    <p><span class=" font-semibold">However</span>, it is currently under <span class="font-noto-serif">construction</span></p>
+    <div>
+      <p>- Henry B</p>
+    </div>
+  </div>
+
+</body>
+
+</html>
