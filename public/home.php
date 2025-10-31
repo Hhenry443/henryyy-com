14c83487191020a63936bc0bd71dced71773ae4d
Henry Barnes <henrytbarnes07@gmail.com>
2025-10-31 10:57:00 +0000

    started work on the home page

--- public/home.php
+++ public/home.php
@@ -9,16 +9,23 @@
   <title>Henry's Webhome</title>
 </head>
 
-<body class="bg-[#F2F0EF]">
+<body class="bg-[#F2F0EF] flex justify-center">
 
-  <div class='w-full h-screen flex flex-col items-center justify-center text-xl space-y-2 text-gray-600'>
-    <p class="text-6xl">🚧</p>
-    <h1 class="font-semibold">Hello!</h1>
-    <p>This is <span class="font-noto-serif">going</span> to be the <span class='text-blue-500 font-bold font-noto-serif'>BEST</span> website!</p>
-    <p><span class=" font-semibold">However</span>, it is currently under <span class="font-noto-serif">construction</span></p>
-    <div>
-      <p>- Henry B</p>
-    </div>
+  <div class='w-43/50 bg-red-300 h-screen flex flex-col items-center text-xl space-y-2 text-gray-600'>
+    <nav class="bg-blue-300 py-4 w-full flex">
+      <div class="flex items-center justify-start bg-[#F2F0EF] px-1 py-1 rounded-lg border border-">
+        <a class="rounded py-1 px-2 text-sm text-[#18181B]">Henry</a>
+        <a class="rounded py-1 px-2 text-sm text-[#18181B]">Projects</a>
+        <a class="rounded py-1 px-2 text-sm text-[#18181B]">Blog</a>
+        <a class="rounded py-1 px-2 text-sm text-[#18181B]">Watching</a>
+        <a class="rounded py-1 px-2 text-sm text-[#18181B]">Hobbies</a>
+      </div>
+    </nav>
+
+    <p class="text-6xl font-montserrat">Montserrat</p>
+    <p class="text-6xl font-lora">Lora</p>
+    <p class="text-6xl font-hind-madurai">Hind Madurai</p>
+    <p class="text-6xl">Default</p>
   </div>
 
 </body>
